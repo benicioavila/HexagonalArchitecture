f2a173f110921a2273b3d797e0441bdb77eec274
Benicio Ávila <benicio.avila@gmail.com>
2024-02-16 16:35:36 -0300

    example polimorphism with interface

--- src/app/fundamentos/polimorfismo.ts
+++ src/app/fundamentos/polimorfismo.ts
@@ -1,13 +1,41 @@
+import Carro from "@/core/fundamentos/Carro";
 import TerminalUtil from "../util/TerminalUtil";
+import Ferrari from "@/core/fundamentos/Ferrari";
+import Fusca from "@/core/fundamentos/Ferrari";
+import { Terminal } from "terminal-kit";
 
 export default async function polimorfirmo() {
-    TerminalUtil.titulo("Polimorfismo")
-    const tipoCarro = await TerminalUtil.selecao('Tipo de carro?', ['Ferrari', 'Fusca'])
+  TerminalUtil.titulo("Polimorfismo");
+  const [tipoCarro] = await TerminalUtil.selecao(
+    "Tipo de carro?",
+    ["Ferrari", "Fusca"]
+  );
+  const carro: Carro =
+    tipoCarro === 0 ? new Ferrari() : new Fusca();
 
-    while(true){
-        TerminalUtil.limpar()
-      const continuar = await TerminalUtil.confirmacao('Deseja continuar?')
-      if(!continuar) return
-    }
-    
+  while (true) {
+    TerminalUtil.limpar();
+    TerminalUtil.exibirChaveValor(
+      "Velocidade Máxima: ",
+      `${carro.velocidadeMaxima} km/h`
+    );
+
+    TerminalUtil.exibirChaveValor(
+      "Velocidade Atual: ",
+      `${carro.velocidadeAtual} km/h`
+    );
+
+    const [opcao] = await TerminalUtil.selecao(
+      "Qual opção?",
+      ["Acelrar", "Frear"]
+    );
+
+    opcao === 0 ? carro.acelerar() : carro.frear()
+
+
+    const continuar = await TerminalUtil.confirmacao(
+      "Deseja continuar?"
+    );
+    if (!continuar) return;
+  }
 }
---
+++ src/core/fundamentos/Fusca.ts
@@ -0,0 +1,25 @@
+import Carro from "./Carro";
+
+export default class Fusca implements Carro{
+    constructor(
+        readonly velocidadeMaxima: number = 140,
+        private _velocidadeAtual: number = 0
+    ){}
+
+    get velocidadeAtual(){
+        return this._velocidadeAtual
+    }
+
+    acelerar(): void {
+        this._velocidadeAtual = Math.min(
+            this._velocidadeAtual + 2, 
+            this.velocidadeMaxima
+        )
+    }
+    frear(): void {
+        this._velocidadeAtual = Math.max(
+            this._velocidadeAtual - 2, 
+            0
+        )
+    }
+}
