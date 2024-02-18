16e202c3a4d8fb7d6b9548db62c97b03d18de971
Benicio Ávila <benicio.avila@gmail.com>
2024-02-18 09:17:12 -0300

    Starting example of DIP

---
+++ src/app/fundamentos/dip.ts
@@ -0,0 +1,9 @@
+import corrida from "@/core/fundamentos/corrida";
+import TerminalUtil from "../util/TerminalUtil";
+
+export default async function dip(){
+    TerminalUtil.titulo('DIP')
+    corrida()
+
+    await TerminalUtil.esperarEnter()
+}
--- src/app/menu/menuFundamentos.ts
+++ src/app/menu/menuFundamentos.ts
@@ -1,12 +1,14 @@
 import TerminalUtil from "@/app/util/TerminalUtil";
 import { terminal } from "terminal-kit";
 import polimorfirmo from "../fundamentos/polimorfismo";
+import dip from "../fundamentos/dip";
 
 export default async function menuFundamentos(){
     TerminalUtil.titulo('Fundamentos')
 
     const [indice] = await TerminalUtil.menu([
         '1.polimorfismo',
+        '2. DIP',
         'voltar'
     ])
 
@@ -14,7 +16,11 @@ export default async function menuFundamentos(){
         case 0:
             await polimorfirmo()
             break
-        case 1: return
+        case 1:
+            await dip()
+            break 
+        default:
+            return
 
     }
 
